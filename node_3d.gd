0c3b0d8170a6b3f5e5a9207c92384a85ac379ee7
Adriano Orioli <orioli.adriano@gmail.com>
2024-06-12 00:16:53 +0200

    added behavior closer to my use case

--- node_3d.gd
+++ node_3d.gd
@@ -3,18 +3,26 @@ extends Node3D
 
 const ITERATIONS = 1000
 
-
-func _ready() -> void:
-	for i in ITERATIONS:
-		var mesh := $Meshes/MeshInstance3D.duplicate()
-		$Meshes.add_child(mesh)
-
-		var sprite := $Meshes/MeshInstance3D.duplicate()
-		$Sprites.add_child(sprite)
-
+@export var mesh_instance_scene: PackedScene
+@export var sprite3d_scene: PackedScene
 
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
 
 	$FPSLabel.text = "%d FPS (%.2f mspf)" % [Engine.get_frames_per_second(), 1000.0 / Engine.get_frames_per_second()]
+	$CountLabel.text = "%d Meshes %d Sprites" % [$Meshes.get_child_count(), $Sprites.get_child_count()]
+
+
+
+
+func _on_spawn_mesh_button_pressed():
+	for i in ITERATIONS:
+		var mesh := mesh_instance_scene.instantiate()
+		$Meshes.add_child(mesh)
+
+
+func _on_spawn_sprites_button_pressed():
+	for i in ITERATIONS:
+		var sp := sprite3d_scene.instantiate()
+		$Sprites.add_child(sp)
